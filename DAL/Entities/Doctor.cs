cb71a869cb7f50c7e1e30925744ea7fb09e2c30a
ErnestLab <ernestos33@ukr.net>
2021-05-10 18:59:47 +0300

    First commit

---
+++ DAL/Entities/Doctor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime Birth { get; set; }
+
+        public int HospitalId { get; set; }
+        public virtual Hospital Hospital { get; set; }
+    }
+}
